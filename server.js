68c7018951df68a1d3653613816ae047ac3b401b
Bruce Taylor <bruce@moo.com>
2015-12-05 22:36:53 +0000

    Changed server.js

--- server.js
+++ server.js
@@ -5,12 +5,14 @@ var path = require('path');
 var port = (process.env.PORT || 1251);
 var app = express();
 
+
+app.use(express.static(path.join(__dirname +'/public/')));
+
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + 'public/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 
-app.use(express.static('public/'));
 
 var server = app.listen(port, function () {
     var host = server.address().address;
