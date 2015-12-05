bd588e1ec463e1a3db4c917031edd13af88e9205
Bruce Taylor <bruce@moo.com>
2015-12-05 22:14:15 +0000

    Changed server.js

--- server/server.js
+++ server/server.js
@@ -1,34 +1,14 @@
-/**
- * Module dependencies.
- */
 
-var express = require('require');
+var express = require('express');
 var app = express();
 
-// log requests
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
 
-// express on its own has no notion
-// of a "file". The express.static()
-// middleware checks for a file matching
-// the `req.path` within the directory
-// that you pass it. In this case "GET /js/app.js"
-// will look for "./public/js/app.js".
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
 
-app.use(express.static(__dirname + '/public'));
-
-// if you wanted to "prefix" you may use
-// the mounting feature of Connect, for example
-// "GET /static/js/app.js" instead of "GET /js/app.js".
-// The mount-path "/static" is simply removed before
-// passing control to the express.static() middleware,
-// thus it serves the file correctly by ignoring "/static"
-app.use('/static', express.static(__dirname + '/public'));
-
-// if for some reason you want to serve files from
-// several directories, you can use express.static()
-// multiple times! Here we're passing "./public/css",
-// this will allow "GET /style.css" instead of "GET /css/style.css":
-app.use(express.static(__dirname + '/public/css'));
-
-app.listen(1251);
-console.log('Booted up');
+    console.log('Example app listening at http://%s:%s', host, port);
+});
