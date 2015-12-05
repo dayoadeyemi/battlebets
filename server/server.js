02f8a04abc4d0a0cedf4fc545ef16d8138f00119
Bruce Taylor <bruce@moo.com>
2015-12-05 22:20:34 +0000

    Changed server.js

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-
+// DEPENDENCIES
+// ============
 var express = require('express');
+var port = (process.env.PORT || 1251);
 var app = express();
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+
+app.use(express.static('public'));
+
+var server = app.listen(port, function () {
     var host = server.address().address;
     var port = server.address().port;
 
