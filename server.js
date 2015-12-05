c84891bffb04be0131a3cbe63026169fec9a6d72
Bruce Taylor <bruce@moo.com>
2015-12-05 22:42:00 +0000

    Changed server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var path = require('path');
 var port = (process.env.PORT || 1251);
 var app = express();
 
+console.log("START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+console.log(__dirname);
 
 app.use(express.static(path.join(__dirname +'/public/')));
 
