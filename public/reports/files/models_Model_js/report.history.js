3c8c8ec5081fa4548fc5520879935f36f0f167e8
Bruce Taylor <bruce@moo.com>
2015-12-05 23:38:02 +0000

    Changed server.js

--- public/reports/files/models_Model_js/report.history.js
+++ public/reports/files/models_Model_js/report.history.js
@@ -1,1 +1,1 @@
-__history = [{"date":"Tue, 18 Feb 2014 05:02:09 GMT","sloc":30,"lloc":6,"functions":3,"deliveredBugs":0.05270780907334513,"maintainability":84.10174117877301,"lintErrors":0,"difficulty":7.5}]
+__history = [{"date":"Tue, 18 Feb 2014 05:02:09 GMT","sloc":30,"lloc":6,"functions":3,"deliveredBugs":0.05270780907334513,"maintainability":84.10174117877301,"lintErrors":0,"difficulty":7.5},{"date":"Sat, 05 Dec 2015 23:15:02 GMT","sloc":30,"lloc":6,"functions":3,"deliveredBugs":0.05270780907334513,"maintainability":84.10174117877301,"lintErrors":0,"difficulty":7.5}]
--- server.js
+++ server.js
@@ -5,10 +5,9 @@ var path = require('path');
 var port = (process.env.PORT || 1251);
 var app = express();
 
-console.log("START >>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 console.log("|||||||||||||||||      " + __dirname);
 
-app.use(express.static(path.join(__dirname +'/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
